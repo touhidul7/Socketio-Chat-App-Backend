13992fd56f12d615bb8891505dbda2f561882bca
touhidul-islam-html <ti360109@gmail.com>
2024-08-01 03:10:19 +0600

    update

--- db.js
+++ db.js
@@ -1,13 +1,32 @@
-const mongoose = require('mongoose')
+/* const mongoose = require('mongoose')
 function Connection() {
-    const mongoURI = "mongodb+srv://begger-chat:begger-chat@begger-chat.d8m70wg.mongodb.net/mymongodb?"
+    const mongoURI = "mongodb+srv://begger-chat:begger-chat@begger-chat.d8m70wg.mongodb.net/mymongodb?retryWrites=true&w=majority&appName=begger-chat" 
 
-
-    /* "mongodb+srv://touhidul5700:reactchatmass@reactchatmass.srwveie.mongodb.net/"  *//* "mongodb://localhost:27017/" */ /* "mongodb+srv://touhidul5700:rsMGZBLzWJZBSDr1@cluster0.c48j5ub.mongodb.net/" */
 
     mongoose.connect(mongoURI)
         .then(() => console.log("connected"))
         .catch(err => console.log(err))
 }
 
-module.exports = Connection
+module.exports = Connection */
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+function Connection() {
+    const mongoURI = process.env.MONGO_URI;
+
+    if (!mongoURI) {
+        console.error("MONGO_URI is not defined in environment variables");
+        return;
+    }
+
+    mongoose.connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB connected successfully"))
+    .catch(err => console.error("MongoDB connection error:", err));
+}
+
+module.exports = Connection;
