1deca18072f7e5533675f55eea47e579a2b8bff5
touhidul-islam-html <ti360109@gmail.com>
2024-08-01 02:37:37 +0600

    update database

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 function Connection() {
-    const mongoURI = "mongodb+srv://begger-chat:begger-chat@begger-chat.d8m70wg.mongodb.net/mymongodb?retryWrites=true&w=majority&appName=begger-chat"
+    const mongoURI = "mongodb+srv://begger-chat:begger-chat@begger-chat.d8m70wg.mongodb.net/mymongodb?"
 
 
     /* "mongodb+srv://touhidul5700:reactchatmass@reactchatmass.srwveie.mongodb.net/"  *//* "mongodb://localhost:27017/" */ /* "mongodb+srv://touhidul5700:rsMGZBLzWJZBSDr1@cluster0.c48j5ub.mongodb.net/" */
