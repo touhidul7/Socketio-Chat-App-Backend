1cd16fec540b58ff1990a101df297720bf9c13c2
touhidul-islam-html <ti360109@gmail.com>
2024-08-01 01:55:37 +0600

    vercel.json

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 function Connection() {
-    const mongoURI = "mongodb+srv://touhidul5700:rsMGZBLzWJZBSDr1@cluster0.c48j5ub.mongodb.net/"
+    const mongoURI ="mongodb+srv://touhidul5700:reactchatmass@reactchatmass.srwveie.mongodb.net/" /* "mongodb://localhost:27017/" */ /* "mongodb+srv://touhidul5700:rsMGZBLzWJZBSDr1@cluster0.c48j5ub.mongodb.net/" */
+    
     mongoose.connect(mongoURI)
     .then(() => console.log("connected"))
     .catch(err => console.log(err))
